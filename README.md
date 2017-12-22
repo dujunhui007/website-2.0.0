772c565ddcc8fa671238866a0df9eeaa5a55fbfa
dujunhui007 <810743626@qq.com>
2017-12-22 17:59:34 +0800

    test asch website

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# website-2.0.0
+# asch-new-Website
--- js/index.js
+++ js/index.js
@@ -1,12 +1,23 @@
 $(document).ready(
-  // $('.deScroll').css('overflow', 'hidden'),
-  $(".pageDown").click(function () {
-    // $('.deScroll').css('overflow', 'auto')
-  }),
+  (function () {
+    var userAgent = navigator.userAgent;
+    var isEdge = userAgent.indexOf("Edge");
+    var isIE = userAgent.indexOf("MSIE");
+    // userAgent.indexOf("compatible") > -1 && userAgent.indexOf("MSIE") > -1 && !isOpera)
+    if (isEdge > -1) {
+      $("#home .aschLogo").addClass("homeLogoAmimateTwo").removeClass("homeLogoAmimateOne");
+    } else if (!!window.ActiveXObject || "ActiveXObject" in window) {
+      $("#home .aschLogo").addClass("homeLogoAmimateTwo").removeClass("homeLogoAmimateOne");
+    } else {
+      $("#home .aschLogo").addClass("homeLogoAmimateOne").removeClass("homeLogoAmimateTwo");
+    }
+  })(),
+
+
   $(window).scroll(function () {
     var winHeight = $(this).height();
     var winWidth = $(this).width();
-    var homeHeight=$("home").height();
+    var homeHeight = $("home").height();
     var hrLine = $("#advantages hr");
     var sTop = $(this).scrollTop();
     var hrLineTop = $(hrLine).offset().top;
@@ -14,27 +25,46 @@ $(document).ready(
       $("#advantages hr").addClass("hrAnimate")
     }
 
-    if($(window).scrollTop()>(homeHeight+200)){
-      $(".toTop").css("display","block")
-    }else {
-      $(".toTop").css("display","none")
+    if ($(window).scrollTop() > (homeHeight + 200)) {
+      $(".toTop").css("display", "block")
+    } else {
+      $(".toTop").css("display", "none")
     }
 
 
-
     if ($(window).scrollTop() > 0) {
-      $("nav").css("background", "#fff").css("box-shadow","0 0 5px #ccc");
+      $("nav").css("background", "#fff").css("box-shadow", "0 0 5px #ccc");
       $("nav .navRight .downloadWhitePaper").addClass("downloadWhitePaperTwo").removeClass("downloadWhitePaperOne");
       $("nav .navLeft  .navLogo").addClass("replaceLogoOne").removeClass("replaceLogoTwo");
       $("nav .equalThan ").addClass("moreThan").removeClass("equalThan");
     } else {
-      $("nav").css("background", "transparent").css("box-shadow","0 0 0");
+      $("nav").css("background", "transparent").css("box-shadow", "0 0 0");
       $("nav .navRight .downloadWhitePaper").addClass("downloadWhitePaperOne").removeClass("downloadWhitePaperTwo");
       // $("nav .navRight .downloadWhitePaper").css("color", "#1c49ae");
       $("nav .navLeft  .navLogo").addClass("replaceLogoTwo").removeClass("replaceLogoOne");
       $("nav .moreThan ").addClass("equalThan").removeClass("moreThan");
 
     }
+  }),
+
+  $(".homeLinks .toGit").click(function () {
+    // window.location.href = "aschVideo.html";
+    window.open("https://github.com/AschPlatform");
+  }),
+
+  $(".homeLinks .toExplore").click(function () {
+    // window.location.href = "aschVideo.html";
+    window.open("https://explorer.asch.io/");
+  }),
+
+  $(".homeLinks .toWallet").click(function () {
+    // window.location.href = "aschVideo.html";
+    window.open("https://mainnet.asch.io/#/home");
+  }),
+
+  $(".homeLinks .homeToVideo").click(function () {
+    // window.location.href = "aschVideo.html";
+    window.open("http://bbs.asch.io/");
   }),
 
   $("#downloads  .dlist").on("click", "li", function () {
@@ -46,13 +76,9 @@ $(document).ready(
     $(this).parents(".downloadsContainerLeft").find(".dlsit-list li").eq(index).show().siblings().hide();
   }),
 
-  // $(".homeLinks").click(function () {
-  //   window.location.href = "aschVideo.html";
-  // }),
 
   $("#introduction .aschVideo").click(function () {
     // window.location.href = "aschVideo.html";
     window.open("aschVideo.html");
   })
-
 );
