77ddb573e93d2b1546a0e454a5bbb6050f0b6b31
dujunhui007 <810743626@qq.com>
2017-12-25 23:57:56 +0800

    add video to homg

--- js/videoPaly.js
+++ js/videoPaly.js
@@ -1,23 +1,33 @@
 $(document).ready(function () {
-  setTimeout(function () {
-    var myPlayer = videojs('my-video');
-    videojs("my-video").ready(function () {
-      var myPlayer = this;
-      myPlayer.play();
-      $("#asch-video").css("display", "none");
-      $("#my-video").css("display", "block")
-    });
-  }, 0);
+  $("#introduction .aschVideo").click(function () {
+    $(document).bind('mousewheel', function(event, delta) { return false; });
+    // alert(1);
+    $(".m").css("display","block");
+    setTimeout(function () {
+      var myPlayer = videojs('my-video');
+      videojs("my-video").ready(function () {
+        var myPlayer = this;
+        myPlayer.play();
+        $("#asch-video").css("display", "none");
+        $("#my-video").css("display", "block")
+      });
+    }, 1000);
 
-  setTimeout(function () {
-    var myPlayer = videojs('asch-video');
-    videojs("asch-video").ready(function () {
-      var myPlayer = this;
-      myPlayer.play();
-      $("#asch-video").css("display", "block");
-      $("#my-video").css("display", "none")
-    });
-  }, 19000);
+    setTimeout(function () {
+      var myPlayer = videojs('asch-video');
+      videojs("asch-video").ready(function () {
+        var myPlayer = this;
+        myPlayer.play();
+        $("#asch-video").css("display", "block");
+        $("#my-video").css("display", "none")
+      });
+    }, 19000);
+  });
+  $(".m .closedVideo").click(function () {
+    $(document).unbind('mousewheel');
+    $(".m").css("display","none")
+  })
+
 });
 
 
