cf8b4248d1041691d6ac9188415b1dda8c6b502e
dujunhui007 <810743626@qq.com>
2018-01-02 14:50:47 +0800

    change small

--- js/videoPaly.js
+++ js/videoPaly.js
@@ -3,9 +3,11 @@ $(document).ready(function () {
   var isEdge = userAgent.indexOf("Edge");
   var isIE = userAgent.indexOf("MSIE");
   var isQQBrowser, is360;
+  var isAndroid;
 
   isQQBrowser = window.navigator.userAgent.indexOf("QQBrowser") !== -1;
   is360 = _mime("type", "application/vnd.chromium.remoting-viewer");
+  isAndroid = window.navigator.userAgent.indexOf('Android');
 
   function _mime(option, value) {
     var mimeTypes = navigator.mimeTypes;
@@ -19,46 +21,46 @@ $(document).ready(function () {
 
   $("#introduction .aschVideo").click(function () {
     // $("#introduction .introductionContainer .aschVideo").click(function () {
-      if (isEdge > -1) {
-        $("#introduction .videoContainer").css("display", "none");
-        window.open("aschVideo.html");
-      } else if (!!window.ActiveXObject || "ActiveXObject" in window) {
-        $("#introduction .videoContainer").css("display", "none");
-        window.open("aschVideo.html");
-      } else {
-        $("#introduction .videoContainer").css("display", "block");
-        setTimeout(function () {
-          $(".m .closedVideo").css("display", "block")
-        }, 1000);
+    if (isEdge > -1) {
+      $("#introduction .videoContainer").css("display", "none");
+      window.open("aschVideo.html");
+    } else if (!!window.ActiveXObject || "ActiveXObject" in window) {
+      $("#introduction .videoContainer").css("display", "none");
+      window.open("aschVideo.html");
+    } else if (isAndroid > -1) {
+      $("#introduction .videoContainer").css("display", "none");
+    } else {
+      $("#introduction .videoContainer").css("display", "block");
+      setTimeout(function () {
+        $(".m .closedVideo").css("display", "block")
+      }, 1000);
 
-        setTimeout(function () {
-          $(document).bind('mousewheel', function (event, delta) {
-            return false;
-          });
-          $(".m").show();
-          $(".m").removeClass("mClosedVideo").addClass("mOpenVideo");
-          $(".vjs-has-started .vjs-control-bar").css("display", "none");
-          $(".videoBackground").css("display", "block")
-          // $(".videoBackground").css("display", "block").removeClass("videoBackgroundAnimate2").addClass("videoBackgroundAnimate1");
-        }, 0);
+      setTimeout(function () {
+        $(document).bind('mousewheel', function (event, delta) {
+          return false;
+        });
+        $(".m").show();
+        $(".m").removeClass("mClosedVideo").addClass("mOpenVideo");
+        $(".vjs-has-started .vjs-control-bar").css("display", "none");
+        $(".videoBackground").css("display", "block")
+        // $(".videoBackground").css("display", "block").removeClass("videoBackgroundAnimate2").addClass("videoBackgroundAnimate1");
+      }, 0);
 
-        setTimeout(function () {
-          var myPlayer = videojs('my-video');
-          videojs("my-video").ready(function () {
-            var myPlayer = this;
-            myPlayer.play();
-            $("#asch-video").css("display", "none");
-            $("#my-video").css("display", "block")
-          });
-        }, 100);
+      setTimeout(function () {
+        var myPlayer = videojs('my-video');
+        videojs("my-video").ready(function () {
+          var myPlayer = this;
+          myPlayer.play();
+          $("#asch-video").css("display", "none");
+          $("#my-video").css("display", "block")
+        });
+      }, 100);
 
-        setTimeout(function () {
-          $(".vjs-has-started .vjs-control-bar").css("display", "flex");
-        }, 1000);
-      }
+      setTimeout(function () {
+        $(".vjs-has-started .vjs-control-bar").css("display", "flex");
+      }, 1000);
+    }
     // });
-
-
 
 
     // setTimeout(function () {
